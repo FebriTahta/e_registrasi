993f67ac2f9dedb2db30e5367c85e037e6e79909
FebriTahta <febrirtah@gmail.com>
2021-11-24 15:29:54 +0700

    hapus limit

--- resources/views/online_registrasi.blade.php
+++ resources/views/online_registrasi.blade.php
@@ -281,20 +281,20 @@
                         <div class="row">
 							<small style="margin-bottom: 5px">
 								@if ($diklat->jenis == 'diklat')
-                                    Alamat Pengiriman Syahadah (nama jalan max 14 huruf)
+                                    Alamat Pengiriman Syahadah 
 								@else
-									Alamat (nama jalan)
+									Alamat 
                                 @endif 
 							</small>
                             <div class="form-floating form-group col-md-12" style="margin-bottom: 10px">
-                                <textarea name="alamat" maxlength="45" id="alamat" cols="30" class="form-control required" rows="2"></textarea>
+                                <textarea name="alamat" id="alamat" cols="30" class="form-control required" rows="2"></textarea>
 								<label for="alamat">Contoh : Jl. Imam Bonjol Block M / No.21</label>
                             </div>
 							@if ($diklat->jenis == "diklat")
-								<small style="margin-bottom: 5px">Alamat pengiriman modul</small>
+								<small style="margin-bottom: 5px">Alamat lengkap pengiriman Modul/Buku</small>
 								<div class="form-floating form-group col-md-12" style="margin-bottom: 10px">
 									<textarea name="alamatx" id="alamatx" cols="30" class="form-control" rows="2"></textarea>
-									<label for="alamatx">Contoh : jika sama boleh dikosongi</label>
+									<label for="alamatx">Contoh : jika sama dengan pengiriman syahadah boleh dikosongi</label>
 								</div>
 							@endif
                         </div>
