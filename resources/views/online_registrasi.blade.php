7ff4062d5b564ac2c47ef6d34bdccda3b0469ade
FebriTahta <febrirtah@gmail.com>
2022-01-26 10:49:36 +0700

    pendaftaran akan segera dibua

--- resources/views/online_registrasi.blade.php
+++ resources/views/online_registrasi.blade.php
@@ -164,10 +164,10 @@
 							<small>- {{Carbon\Carbon::parse($diklat->sampai_tanggal)->isoFormat('dddd, D MMMM Y')}}</small>
 							@endif
 							<br>
+							@if ($diklat->max !== null)
+							<small class="text-success text-uppercase">Kuota {{$diklat->peserta->where('status', 1)->count()}} dari {{$diklat->max}} PESERTA</small>
+							@endif
                         </div><hr>
-						@if ($diklat->max !== null)
-						<small class="text-success text-uppercase"><b>KUOTA PENDAFTARAN : {{$diklat->peserta->where('status', 1)->count()}} dari {{$diklat->max}} PESERTA</b></small>
-						@endif
 						<br>
 						<small style="text-transform: uppercase" class="text-danger">
 							<b>BATAS PENDAFTARAN :
